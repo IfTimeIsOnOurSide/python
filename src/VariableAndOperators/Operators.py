88006adbdaf12f79456c6e4acd058b935b9cfd6d
1970 <1580426939@qq.com>
2019-04-25 10:10:14 +0800

    变量类型

---
+++ src/VariableAndOperators/Operators.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# @Site    : 运算符
+# @File    : Operators.py
+# @Author  : daimin
+# @Time    : 2019/4/24 22:41
+# @Desc     :
---
+++ src/VariableAndOperators/Variable.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# @Site    : 变量类型
+# @File    : Variable.py
+# @Author  : daimin
+# @Time    : 2019/4/24 22:40
+# @Desc     :
+
+'''
+弱类型：
+1. 无序声明即可使用；
+2. 类型可变；
+'''
+
+name = 'daimin'
+print(type(name))
+# print('type='+ type(name) + ', name=' + name)
